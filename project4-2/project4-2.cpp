fa22952be4c44463cab56f3515e7ddf25739fe3f
cuellarj <j.cue@hotmail.com>
2016-05-25 15:49:20 -0700

    Adding 2-opt methods

--- project4-2/project4-2.cpp
+++ project4-2/project4-2.cpp
@@ -9,11 +9,34 @@
 #include <limits>
 #include <algorithm>
 
+/*****************************************************************/
+/*****************************************************************/
+//FUNCTION decVarLARATION
+/*****************************************************************/
+/*****************************************************************/
 void get_input(char* fName, std::vector< std::vector<int> >& matrix);
-std::vector< std::vector<int> > randDistMatrix(int numCities);
 std::vector<int> nnTour(std::vector< std::vector<int> > distanceMatrix, int numCities);
-int getCost(std::vector< std::vector<int> > distanceMatrix, std::vector<int> tour);
+int calculateCost(std::vector< std::vector<int> > distanceMatrix, std::vector<int> tour);
+std::vector<int> two_opt(std::vector<int> nnTour, std::vector< std::vector<int> > distanceMatrix, int numCities, clock_t timer);
+std::vector<int> two_optSwap(std::vector<int> nnTour, int swapFront, int swapEnd, int numCities);
 
+/*****************************************************************/
+/*****************************************************************/
+//CLASS decVarLARATION
+/*****************************************************************/
+/*****************************************************************/
+class point{
+  public:
+    int number;
+    int x;
+    int y;
+};
+
+/*****************************************************************/
+/*****************************************************************/
+//MAIN FUNCTION
+/*****************************************************************/
+/*****************************************************************/
 int main(int argc, char* argv[])
 {
 	if(argc < 2)
@@ -22,6 +45,7 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
+	//for testing
   	clock_t startTime, endTime;
   	startTime = clock();
 	
@@ -33,47 +57,66 @@ int main(int argc, char* argv[])
 	//Get nearest neighbor tour
 	std::vector<int> tour = nnTour(distanceMatrix, numCities);
 
+	//Output tour
+	std::cout << "Nearest Neighbor Tour" << std::endl;
+	for(int i = 0; i <= numCities-1; i++)
+	{
+		std::cout << tour[i] << '\t';
+	}
+	std::cout << std::endl;
+
+	//Get and output cost
+	int cost = calculateCost(distanceMatrix, tour);
+	std::cout << "Total cost: " << cost << std::endl;
+
+	//Get 2opt tour
+	std::vector<int> two_optTour = two_opt(tour, distanceMatrix, numCities, startTime);
+
 	//Open output file
 	std::ofstream file;
 	std::string fName = argv[1];
 	fName += ".tour";
 	file.open(fName);
-	
-	int cost = getCost(distanceMatrix, tour);
+
+	//Get and output cost
+	cost = calculateCost(distanceMatrix, two_optTour);
 	file << cost << '\n';
-	
+
 	//Output tour
-	std::cout << "Traveling Salesman Tour" << std::endl;
+	std::cout << "After 2-opt and 2-opt Swap" << std::endl;
 	for(int i = 0; i <= numCities-1; i++)
 	{
-		std::cout << tour[i] << '\t';
-		file << tour[i] << '\n';
+		std::cout << two_optTour[i] << '\t';
+		file << two_optTour[i] << '\n';
 	}
-	file.close();
 	std::cout << std::endl;
+
+  	file.close();
 	
-	std::cout << "Total cost: " << cost << std::endl;
-	
-	
+	//for testing
 	endTime = clock();
 	endTime -= startTime;
-	
+
+	//Output cost
+	cost = calculateCost(distanceMatrix, two_optTour);
+	std::cout << "Total cost: " << cost << std::endl;
+
 	return 0;
 }
+/*****************************************************************/
+/*****************************************************************/
+//ALGORITHMS AND HELPER FUNCTIONS
+/*****************************************************************/
+/*****************************************************************/
 
-class point{
-  public:
-    int number;
-    int x;
-    int y;
-};
 //Putting file data into a point matrix
 void get_input(char* fName, std::vector< std::vector<int> >& matrix){
-  std::vector<point> pointList;
-  std::ifstream file;
-  std::string line;
-  file.open(fName);
-  while (std::getline(file, line)){
+	std::vector<point> pointList;
+	std::ifstream file;
+	std::string line;
+	file.open(fName);
+	
+	while (std::getline(file, line)){
     std::istringstream iss(line);
     point a;
     iss >> a.number;
@@ -86,7 +129,7 @@ void get_input(char* fName, std::vector< std::vector<int> >& matrix){
   // Turns list of points into relative distance matrix
   int numPoints = pointList.size();
   matrix.resize(numPoints, std::vector<int>(numPoints, -1));
-
+  
   for (int a = 0; a < numPoints; a++){
     for (int b = 0; b < numPoints; b++){
       matrix[a][b] = round(sqrt((pointList[a].x - pointList[b].x)*(pointList[a].x - pointList[b].x) + (pointList[a].y - pointList[b].y)*(pointList[a].y - pointList[b].y)));
@@ -94,36 +137,14 @@ void get_input(char* fName, std::vector< std::vector<int> >& matrix){
   }
 }
 
-
-std::vector< std::vector<int> > randDistMatrix(int numCities)
-{
-	std::vector< std::vector<int> > distanceMatrix;
-	distanceMatrix.resize(numCities, std::vector<int>(numCities, 0));
-	int randNum;
-
-	for(int i = 0; i < numCities; i++)
-	{
-		for(int j = 0; j <= i; j++)
-		{
-			randNum = std::rand() % 200 + 1;
-			if(i == j)
-				randNum = -1;
-			distanceMatrix[i][j] = randNum;
-			distanceMatrix[j][i] = randNum;
-		}
-	}
-
-	return distanceMatrix;
-}
-
-
+//Salesman tour solver using neighest neighbor method. 
+//Source: https://en.wikipedia.org/wiki/Nearest_neighbour_algorithm
 std::vector<int> nnTour(std::vector< std::vector<int> > distanceMatrix, int numCities)
 {
 	int currentCity, nextCity;
 	int minDistance;
 	std::vector<int> tour;
-	tour.resize(numCities + 1, -1);
-		
+	tour.resize(numCities + 1, -1);	
 	currentCity = 0;
 
 	for(int j = 0; j < numCities; j++)
@@ -140,18 +161,88 @@ std::vector<int> nnTour(std::vector< std::vector<int> > distanceMatrix, int numC
 		}
 		currentCity = nextCity;
 	}
-
 	tour[numCities] = 0;	
 	return tour;
 }
 
+//We pass the nearest neighbor solved matrix into the 2-opt and 2-optSwap functions to find a more precise function.
+//Source: https://en.wikipedia.org/wiki/2-opt
+//Source: http://www.technical-recipes.com/2012/applying-c-implementations-of-2-opt-to-travelling-salesman-problems/
+std::vector<int> two_opt(std::vector<int> nnTour, std::vector< std::vector<int> > distanceMatrix, int numCities, clock_t timer)
+{
+	
+	int size = nnTour.size();
+	bool improve = false;
+	std::vector<int> finalTour = nnTour;
+  	clock_t max_time = CLOCKS_PER_SEC * 15;
+  	clock_t loop_time;
+  	clock_t current_time;
+	
+	while (!improve && timer < max_time)
+	{
+		int best_dist = calculateCost(distanceMatrix, finalTour);
+		for (int i = 1; i < size - 2; i++)
+		{
+			for (int k = i+1; k < size - 1; k++)
+			{
+				std::vector<int> newTour = two_optSwap(finalTour, i, k, numCities);
+				int new_dist = calculateCost(distanceMatrix, newTour);
 
-int getCost(std::vector< std::vector<int> > distanceMatrix, std::vector<int> tour)
+				if(new_dist < best_dist)
+				{
+
+					improve = false;
+					finalTour = newTour;
+					best_dist = new_dist;
+				}
+
+				current_time = clock();
+				loop_time = current_time - timer;
+				if(loop_time > max_time)
+				{
+					std::cout << "\nTime to complete Neighest Neighbor and 2-Opt(sec): " << (loop_time / CLOCKS_PER_SEC) << std::endl;
+					return finalTour;
+				}
+			}
+		}
+		improve = true;
+	}
+  	std::cout << "\nTime to complete Neighest Neighbor and 2-Opt (sec): " << (loop_time / CLOCKS_PER_SEC) << std::endl;
+	return finalTour;
+}
+
+//Second part of 2-opt solution. This will swap tour stops if one provides a lower cost.
+//Same sources as above.
+std::vector<int> two_optSwap(std::vector<int> nnTour, int swapFront, int swapEnd, int numCities)
+{
+	int size = nnTour.size();
+	std::vector<int> newTour = nnTour;
+
+	for (int i = 0; i < swapFront - 1; i++)
+	{
+		newTour[i] = nnTour[i];
+	}
+
+	int decVar = 0;
+	for (int i = swapFront; i <= swapEnd; i++)
+	{
+		newTour[i] = nnTour[swapEnd - decVar];
+		decVar++;
+	}
+
+	for (int i = swapEnd + 1; i < size; i++)
+	{
+		newTour[i] = nnTour[i];
+	}
+	return newTour;
+}
+
+//Function to calculate total cost
+int calculateCost(std::vector< std::vector<int> > distanceMatrix, std::vector<int> tour)
 {
 	int cost, totalCost;
 	int tourLength = tour.size();
 	int currentCity, nextCity;
-
 	totalCost = 0;
 
 	for(int i = 0; i < tourLength - 1; i++)
@@ -161,6 +252,5 @@ int getCost(std::vector< std::vector<int> > distanceMatrix, std::vector<int> tou
 		cost = distanceMatrix[currentCity][nextCity];
 		totalCost += cost;
 	}
-
 	return totalCost;
 }
